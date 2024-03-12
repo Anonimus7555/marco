8f2c9d6c68322acfe1f47fefb9c1ca7f55c756cd
Anonimus7555 <marcodiaz7555@gmail.com>
2024-03-12 11:34:02 -0500

    primer add

---
+++ condicional.py
@@ -0,0 +1,7 @@
+numero = 10
+
+if numero >= 5 and numero <= 10:
+    print ("Buena nota")
+    
+else:
+    print ("DESAPROBADO!!!!")
---
+++ condicional2.py
@@ -0,0 +1,13 @@
+codigo = 2
+
+if codigo == 1:
+    print ("Es ingeniero")
+    
+elif codigo == 2:
+    print ("Es medico")
+    
+elif codigo == 3:
+    print ("Es abogado")
+    
+else:
+    print ("NO HAY UNA CARRERA CON ESE CODIGO ")
